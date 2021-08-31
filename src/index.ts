5b94e593138d30bff70679bc89e500ff2df10d61
SungYeolha <tjddufgk@gmail.com>
2021-08-31 17:50:49 +0900

    💙Add local typescript & tsc-watch

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello! I'm ${name}, I'm ${age}, I'm ${gender}`
+
+}
+
+console.log(sayHi("Hotsumm", 27, "male"))
+export { }
